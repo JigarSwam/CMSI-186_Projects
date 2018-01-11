7705fea8251a322d8923f944ad656cba77274f15
JigarSwam <jswaminarayan@gmail.com>
2018-01-11 13:32:44 -0800

    Edit - Added Heading

--- homework00/HelloWorld.java
+++ homework00/HelloWorld.java
@@ -1,3 +1,10 @@
+/*
+Author   : Jigar Swaminarayan
+Date     : 2018-01-11
+Class    : CMSI-186 Programming Lab
+Warnings : None
+*/
+
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println( "Hello, world"  );
