2d19f1d02225456f4301da1fc3506e8273950f71
JigarSwam <jswaminarayan@gmail.com>
2018-01-18 11:47:56 -0800

    Created CalendarStuff.java & CountTheDays.java

---
+++ homework01/CalendarStuff.java
@@ -0,0 +1,25 @@
+
+/*
+Author      : Jigar Swaminarayan
+Date        : 2018-01-18
+Class       : CMSI-186 Programming Lab
+Warnings    : None
+Description : Program which determines leap year, valid dates, days in a month
+              and Number of days between two Dates.
+*/
+
+public class CalendarStuff {
+
+  public static boolean isLeapYear(long year) {
+
+  }
+  public static long daysInMonth(long month, long year) {
+
+  }
+  public static boolean isValidDate(long month, long day, long year) {
+
+  }
+  public static long daysBetween(long month0, long day0, long year0, long month1, long day1, long year1) {
+
+  }
+}
---
+++ homework01/CountTheDays.java
@@ -0,0 +1,12 @@
+
+/*
+Author      : Jigar Swaminarayan
+Date        : 2018-01-18
+Class       : CMSI-186 Programming Lab
+Warnings    : None
+Description : Program which counts number of days between two dates.
+*/
+
+public class CountTheDays {
+
+}
