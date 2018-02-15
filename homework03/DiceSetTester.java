f5d140b3f7d0e4876bc6535164073dd1f4fa5ccf
JigarSwam <jswaminarayan@gmail.com>
2018-02-15 12:49:57 -0800

    Testing class added

---
+++ homework03/DiceSetTester.java
@@ -0,0 +1,33 @@
+/*
+Original Author : Jigar Swaminarayan
+File Name       : DieSetTester.java
+Date            : 2018-02-15
+Class           : CMSI-186 Programming Lab
+Warnings        : None
+Description     : Tests for DieSet.java methods
+*/
+public class DiceSetTester {
+
+  public static void main ( String [] args ) {
+    DiceSet ds = null;
+
+    try{ds = new DiceSet(1, 1);}
+    catch (IllegalArgumentException iae) {System.out.println ("Too few sides requested to constructor...");}
+
+    try{ds = new DiceSet(2, 2);}
+    catch (IllegalArgumentException iae) {System.out.println ("Too few sides requested to constructor...");}
+
+    try{ds = new DiceSet(3, 3);}
+    catch (IllegalArgumentException iae) {System.out.println ("Too few sides requested to constructor...");}
+
+    try{ds = new DiceSet(4, 4);}
+    catch (IllegalArgumentException iae) {System.out.println ("Too few sides or too few die requested to constructor...");}
+    System.out.println("roll() test for a 4 die with 4 sides");
+    System.out.println("You rolled " + ds.toString());
+    System.out.println("Your value is " + ds.sum());
+    System.out.println("An individual role of the third die " + ds.rollIndividual(2));
+    System.out.println("An individual value of that role is " + ds.getIndividual(2));
+    System.out.println("An individual role of the sixth die, should be error " + ds.rollIndividual(5));
+  }
+
+}
