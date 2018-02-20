a4a9817e87212685c3214eeef1a837152acae84c
JigarSwam <jswaminarayan@gmail.com>
2018-02-20 12:15:40 -0800

    Updated HighRoll.java

--- homework03/HighRoll.java
+++ homework03/HighRoll.java
@@ -4,5 +4,60 @@ File Name   : HighRoll.java
 Date        : 2018-01-25
 Class       : CMSI-186 Programming Lab
 Warnings    : None
-Description : <Fill>
+Description : Die rolling game
 */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class HighRoll{
+
+   public static void main( String args[] ) {
+      System.out.println( "\n   Welcome to High Roll!\n" );
+      DiceSet dSet = new DiceSet(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+     // This line uses the two classes to assemble an "input stream" for the user to type
+     // text into the program
+      BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+      while( true ) {
+        System.out.println( "Press the 'q' key to quit the program." );
+        System.out.println("Press '1' to roll all the die! ");
+        System.out.println("Press '2' to role one die. ");
+        System.out.println("Press '3' to calculate the score of all the die. ");
+        System.out.println("Press '4' to save the score as a high score. ");
+        System.out.println("Press '5' to display the high score. ");
+
+         System.out.print( ">> " );
+         String inputLine = null;
+         try {
+            inputLine = input.readLine();
+            if( 0 == inputLine.length() ) {
+               System.out.println("enter some text!:");
+            }
+            System.out.println( "   You typed: " + inputLine );
+            if( 'q' == inputLine.charAt(0) ) {
+               break;
+            }
+            else if ('1' == inputLine.charAt(0)) {
+              dSet.roll();
+            }
+            else if('2' == inputLine.charAt(0)) {
+              dSet.rollIndividual(index);
+            }
+            else if('3' == inputLine.charAt(0)) {
+              dSet.sum();
+            }
+            else if('4' == inputLine.charaAt(0)) {
+              
+            }
+            else if('5' == inputLine.charAt(0)) {
+              dSet.toString();
+            }
+         }
+         catch( IOException ioe ) {
+            System.out.println( "Caught IOException" );
+         }
+      }
+   }
+}
