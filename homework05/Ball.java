919d33f46a8a5f548eb9ad17c17b1b9a139c2377
JigarSwam <jswaminarayan@gmail.com>
2018-03-13 11:43:29 -0700

    Created SoccerSim.java and ball.java

---
+++ homework05/Ball.java
@@ -0,0 +1,11 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SoccerSim.java
+ *  @author       :  Jigar Swaminarayan
+ *  Date written  :  2017-03-13
+ *  Description   :  Helper class for a Ball object for SoccerSim.java.
+ *  Warnings      :  None
+ *  Exceptions    :  None
+**/
+Public class Ball {
+
+}
---
+++ homework05/SoccerSim.java
@@ -0,0 +1,15 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SoccerSim.java
+ *  @author       :  B.J. Johnson
+ *  Modified By   :  Jigar Swaminarayan
+ *  Date written  :  2017-03-13
+ *  Description   :  Simulation with soccer balls to determine, after they are kicked, if there is a collision.
+ *  Warnings      :  None
+ *  Exceptions    :  None
+**/
+public class SoccerSim {
+  private final double RADIUS_IN_INCHES = 4.45;
+  private final double WEIGHT_IN_POUNDS = 1;
+
+
+}
