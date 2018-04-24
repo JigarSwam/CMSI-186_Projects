3fc20486999927c641314706b475396ef54eed66
JigarSwam <jswaminarayan@gmail.com>
2018-04-24 13:15:33 -0700

    minor changes

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -9,8 +9,14 @@
  */
 
 public class DynamicChangeMaker {
+  public static int rowCount = 0;
+  public static int columnCount = 0;
 
   public static String makeChangeWithDynamicProgramming(int[] denominations, int target) {
+    rowCount = denominations.length;
+    columnCount = target + 1;
+    Tuple[][] theTable = new Tuple[rowCount][columnCount];
+
     return "";
   }
   // validating arguments methods
