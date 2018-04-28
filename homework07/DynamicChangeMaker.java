4c6aab832a7f4a41ffa793d432563d2e5507cd7f
JigarSwam <jswaminarayan@gmail.com>
2018-04-28 15:55:00 -0700

    Minor fixes

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -3,7 +3,8 @@
  * Purpose    :  Find the best
  * @author    :  Jigar Swaminarayan
  * Date       :  2017-24-17
- * Description:
+ * Description:  Program to determine the most efficient amount of coins needed to
+ *               add to a target value.
  * Notes      :  None
  * Warnings   :  None
  */
@@ -11,14 +12,14 @@
 public class DynamicChangeMaker {
   public static int rowCount = 0;
   public static int columnCount = 0;
-
+  public static Tuple result;
 
   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    *  Method that takes an integer array of denominations and a integer that is the target value.
    *  @param denominations  Integer Array of the values used to add up to target value.
    *  @param target  Integer value of the intended total from the denominations
    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  public static String makeChangeWithDynamicProgramming(int[] denominations, int target) {
+  public static Tuple makeChangeWithDynamicProgramming(int[] denominations, int target) {
     rowCount = denominations.length;
     columnCount = target + 1;
     Tuple[][] t = new Tuple[rowCount][columnCount];
@@ -31,10 +32,12 @@ public class DynamicChangeMaker {
         } else {
           if(denoms.getElement(i) > j) {
             t[i][j] = Tuple.IMPOSSIBLE;
-            if(t[i][j-denoms.getElement(i)].isImpossible()) {
-              t[i][j] = Tuple.IMPOSSIBLE;
+            if(j >= denoms.getElement(i)) {
+              if(t[i][j-denoms.getElement(i)].isImpossible()) {
+                t[i][j] = Tuple.IMPOSSIBLE;
+              }            
             } else {
-              t[i][j] = t[i][j-denoms.getElement(i)];
+              t[i][j].setElement(i, 1);
             }
             if(i != 0) {
               if(t[i-1][j].isImpossible()) {
@@ -44,28 +47,30 @@ public class DynamicChangeMaker {
               }
             }
           } else {
-          t[i][j].setElement(i, 1);
-           if((j - denominations[i]) >= 0) {
-             if(t[i][j-denoms.getElement(i)].isImpossible()) {
-               t[i][j] = Tuple.IMPOSSIBLE;
-             } else {
-               t[i][j] = t[i][j-1];
-             }
-           }
-           if(i != 0) {
-             // if the cell above is impossible, basically do nothing since
-             if(t[i-1][j].isImpossible()) {
-               t[i][j] = t[i][j];
-             } else if(t[i-1][j].total() < t[i][j].total()) {
-               t[i][j] = t[i-1][j];
-             }
-           }
-         }
-       }
-     }
-   }
-   return "";
- }
+            t[i][j] = new Tuple(denoms.length());
+            t[i][j].setElement(i, 1);
+            if((j - denominations[i]) >= 0) {
+              if(t[i][j-denoms.getElement(i)].isImpossible()) {
+                t[i][j] = Tuple.IMPOSSIBLE;
+              } else {
+                t[i][j] = t[i][j-1];
+              }
+            }
+            if(i != 0) {
+              if(t[i-1][j].isImpossible()) {
+                t[i][j] = t[i][j];
+              } else if(t[i-1][j].total() < t[i][j].total()) {
+                t[i][j] = t[i-1][j];
+              }
+            }
+          }
+        }
+        result = t[i][j];
+      }
+    }
+    return result;
+  }
+  // public String validate() {
+  //   if(denominations)
+  // }
 }
-  // validating arguments methods
-  // helper methods
